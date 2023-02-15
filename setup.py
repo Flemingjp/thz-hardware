157907ee4d311b283d454f7e40c7ac42900a418f
Flemingjp <jamesfleming91@gmail.com>
2023-02-15 15:05:49 +0000

    Included setup.py for modules

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="thzware",
+    version="0.0.1",
+    author="James Fleming",
+    author_email="jamesfleming91@gmail.com",
+    description="Control of THz hardware",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy>=1.23.5",
+        "pycromanager=0.26.0",
+        "nidaqmx=0.6.4",
+    ],
+    python_requires=">=3.10.0"
+)
